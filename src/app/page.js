23d0a00f36c634cec84a64f291e6ef283a5e5d39
Martha Grate <mgrate1020@yahoo.com>
2025-05-30 00:13:32 -0500

    Japanese flag

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,15 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <div>44</div>
-      <ul>
-        <li className={styles.bluey}>Buenisimo</li>
-        <li className={styles.mamamia}>Ricisimo</li>
-      </ul>
-      <h6 className={styles.sammich}>no clicky</h6>
-      <button>click hurr</button>
-      <button>right thurr</button>
+    <div className={styles.flag}>
+      <div className={styles.circle}></div>
     </div>
   );
 }
+
+
 
 
 /*
