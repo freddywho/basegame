f0690b758ee3b94c12bd839042b035605257b0c6
Martha Grate <mgrate1020@yahoo.com>
2025-06-13 08:16:37 -0500

    Alaska state flag

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,17 @@
-import styles from './page.module.css';
+import styles from './page.module.css'; 
 
 export default function HomePage() {
   return (
-    <div className={styles.pageContainer}>
-      <a
-        href="https://www.zillow.com/homedetails/0-L-Kachemak-Bay-15-Homer-AK-99603/448273625_zpid/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={styles.externalLink}
-      >
-        Not Japan
-      </a>
+    <div>
+      <img
+        src="https://www.flagcolorcodes.com/images/jpg/alaska.jpg"
+        alt="Alaska State Flag"
+        className={styles.stateFlag}
+      />
     </div>
   );
 }
+
 
 
 
