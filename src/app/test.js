e777b70b98f7a244a05c2d769a05307cf28ac13f
Martha Grate <mgrate1020@yahoo.com>
2025-06-13 23:49:20 -0500

    tested arrays and default methods

--- src/app/test.js
+++ src/app/test.js
@@ -1,2 +1,11 @@
-var numbers = [2, 2];
-console.log(2*2)
+var restaurantBillsForADay = [2, 5, 9, 12, 2, 199, 4];
+// console.log("billsforaday", restaurantBillsForADay.length) //7
+// .length is a property on the array data type (type of data called an array)
+var cups = "a b cd"
+// console.log("printingcups ...", cups.length) //6 
+var peeps = ["jose", "pedro", "alex"];
+var indexNumber  = 1;
+// console.log("...", peeps[0]) // ... jose
+// console.log(";;;", cups[0]) // ;;; a
+// console.log(peeps[2]) // alex
+console.log(peeps[indexNumber]) // pedro // this is the same as (peeps[1]) with the var indexNumber  = 1;
