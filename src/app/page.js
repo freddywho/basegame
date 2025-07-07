7086478614d7d032058fb0598089e385c9f422f2
Martha Grate <mgrate1020@yahoo.com>
2025-07-06 19:09:59 -0500

    profile card

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,19 @@
-import styles from './page.module.css'; 
+import styles from './page.module.css';
 
-export default function HomePage() {
+export default function Page() {
   return (
-    <div>
+    <div className={styles.card}>
       <img
-        src="https://www.flagcolorcodes.com/images/jpg/alaska.jpg"
-        alt="Alaska State Flag"
-        className={styles.stateFlag}
+        src="/freddywho.jpg"
+        alt="freddywho"
+        className={styles.avatar}
       />
+      <h2 className={styles.name}>freddywho</h2>
+      <p className={styles.title}>Badass Coder</p>
+      <p className={styles.description}>Passionate about a lot of things</p>
     </div>
   );
 }
-
-
-
 
 
 
