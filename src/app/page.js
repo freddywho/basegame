33bb1328b2f9f571fab2ddda1296c9af0f7f0d32
Martha Grate <mgrate1020@yahoo.com>
2025-08-04 10:01:31 -0500

    doubleNumbers function

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,16 @@
-import styles from './page.module.css';
+export default function LearnPage() {
+  function doubleNumbers(array) {
+    array.forEach(element => {
+      if (typeof element === 'number') {
+        console.log(element * 2);
+      }
+    });
+  }
 
-export default function Page() {
-  return (
-    <div className={styles.card}>
-      <img
-        src="/freddywho.jpg"
-        alt="freddywho"
-        className={styles.avatar}
-      />
-      <h2 className={styles.name}>freddywho</h2>
-      <p className={styles.title}>Badass Coder</p>
-      <p className={styles.description}>Passionate about a lot of things</p>
-    </div>
-  );
+doubleNumbers([4, 'blue', 2, 'casa', 8]);
+
+  return <div>Check your terminal for doubled numbers</div>
 }
-
 
 
 
